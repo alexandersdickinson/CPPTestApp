23eaa4b4584a2f8c2ee71c495a47ea678ccb7df6
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2016-02-14 17:39:23 -0800

    Initial Commit

---
+++ TestApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestApp
+//
+//  Created by Alex Dickinson on 14.02.16.
+//  Copyright © 2016 Alex Dickinson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
